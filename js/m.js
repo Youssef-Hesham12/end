f0620dc93bff19c86e238272cd2a0ea45abee86c
Youssef-Hesham123 <youssefhesham1route@gmail.com>
2025-01-10 21:01:36 +0200

    first commit

---
+++ js/m.js
@@ -0,0 +1,120 @@
+// all inputs
+let signupName = document.getElementById('signupName')
+let signupEmail = document.getElementById('signupEmail')
+let signupPassword = document.getElementById('signupPassword')
+let signinEmail = document.getElementById('signinEmail')
+let signinPassword = document.getElementById('signinPassword')
+
+
+// to say welcome in home page
+let username = localStorage.getItem('sessionUsername')
+if (username) {
+    document.getElementById('username').innerHTML = "Welcome " + username
+}
+
+let signUpArray = []
+if (localStorage.getItem('users') == null) {
+    signUpArray = []
+} else {
+    signUpArray = JSON.parse(localStorage.getItem('users'))
+}
+
+
+
+
+//for check inputs is empty or not
+function isEmpty() {
+
+    if (signupName.value == "" || signupEmail.value == "" || signupPassword.value == "") {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+
+
+
+// for check email is exist
+function isEmailExist() {
+    for (let i = 0; i < signUpArray.length; i++) {
+        if (signUpArray[i].email.toLowerCase() == signupEmail.value.toLowerCase()) {
+            return false
+        }
+    }
+}
+
+
+
+
+
+function signUp() {
+    if (isEmpty() == false) {
+        document.getElementById('exist').innerHTML = '<span class="text-danger m-3">All inputs is required</span>'
+        return false
+    }
+    // to store all value as object
+    let signUp = {
+        name: signupName.value,
+        email: signupEmail.value,
+        password: signupPassword.value,
+    }
+    if (signUpArray.length == 0) {
+        signUpArray.push(signUp)
+        localStorage.setItem('users', JSON.stringify(signUpArray))
+        document.getElementById('exist').innerHTML = '<span class="text-success m-3">Success</span>'
+        return true
+    }
+    if (isEmailExist() == false) {
+        document.getElementById('exist').innerHTML = '<span class="text-danger m-3">email already exists</span>'
+
+    } else {
+        signUpArray.push(signUp)
+        localStorage.setItem('users', JSON.stringify(signUpArray))
+        document.getElementById('exist').innerHTML = '<span class="text-success m-3">Success</span>'
+
+    }
+
+
+}
+
+
+
+
+// ============= for login================
+//for check inputs is empty or not
+function isLoginEmpty() {
+
+    if (signinPassword.value == "" || signinEmail.value == "") {
+        return false
+    } else {
+        return true
+    }
+}
+
+function login() {
+    if (isLoginEmpty() == false) {
+        document.getElementById('incorrect').innerHTML = '<span class="text-danger m-3">All inputs is required</span>'
+        return false
+    }
+    let password = signinPassword.value
+    let email = signinEmail.value
+    for (let i = 0; i < signUpArray.length; i++) {
+        if (signUpArray[i].email.toLowerCase() == email.toLowerCase() && signUpArray[i].password.toLowerCase() == password.toLowerCase()) {
+            localStorage.setItem('sessionUsername', signUpArray[i].name)
+            location.href='/Home.html';
+        } else {
+            document.getElementById('incorrect').innerHTML = '<span class="p-2 text-danger">incorrect email or password</span>'
+        }
+    }
+
+}
+
+
+
+
+// for logout
+function logout() {
+    localStorage.removeItem('sessionUsername')
+}
